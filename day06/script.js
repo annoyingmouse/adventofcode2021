8fd8a4d61977c39572a6f66d0ece07f2b1ace5e9
Dominic Myers <Dominic.Myers@spacenk.com>
2021-12-07 17:50:43 +0000

    Day 7 (always read the bloody question - they wanted the fuel used, not the position)

--- day06/script.js
+++ day06/script.js
@@ -1,8 +1,7 @@
 //const original = [3,4,3,1,2]
 const original = [1,4,1,1,1,1,5,1,1,5,1,4,2,5,1,2,3,1,1,1,1,5,4,2,1,1,3,1,1,1,1,1,1,1,2,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,4,1,1,1,1,5,1,4,1,1,4,1,1,1,1,4,1,1,5,5,1,1,1,4,1,1,1,1,1,3,2,1,1,1,1,1,2,3,1,1,2,1,1,1,3,1,1,1,2,1,2,1,1,2,1,1,3,1,1,1,3,3,5,1,4,1,1,5,1,1,4,1,5,3,3,5,1,1,1,4,1,1,1,1,1,1,5,5,1,1,4,1,2,1,1,1,1,2,2,2,1,1,2,2,4,1,1,1,1,3,1,2,3,4,1,1,1,4,4,1,1,1,1,1,1,1,4,2,5,2,1,1,4,1,1,5,1,1,5,1,5,5,1,3,5,1,1,5,1,1,2,2,1,1,1,1,1,1,1,4,3,1,1,4,1,4,1,1,1,1,4,1,4,4,4,3,1,1,3,2,1,1,1,1,1,1,1,4,1,3,1,1,1,1,1,1,1,5,2,4,2,1,4,4,1,5,1,1,3,1,3,1,1,1,1,1,4,2,3,2,1,1,2,1,5,2,1,1,4,1,4,1,1,1,4,4,1,1,1,1,1,1,4,1,1,1,2,1,1,2]
 
-const inputToObj = arr => {
-  return arr.reduce((previousValue, currentValue) => {
+const inputToObj = arr => arr.reduce((previousValue, currentValue) => {
     if(previousValue.hasOwnProperty(currentValue.toString())){
       previousValue[currentValue] += 1
     }else{
@@ -10,31 +9,22 @@ const inputToObj = arr => {
     }
     return previousValue
   }, {})
-}
 
 let input = [...original]
 let inputObj = inputToObj(input)
 
-const process = obj => {
-  const returnObj = {}
-  const arrKeys = Object.keys(obj).map(e => Number(e)).sort().reverse()
-  arrKeys.forEach(value => {
-    if(!value){
-      returnObj[8] = obj[value]
-      if(returnObj[6]){
-        returnObj[6] += obj[value]
-      }else{
-        returnObj[6] = obj[value]
-      }
-    }else{
-      returnObj[value - 1] = obj[value]
-    }
-  })
-  return returnObj
-}
+const process = obj => Object.keys(obj).map(e => Number(e)).sort().reverse().reduce((p, c) => {
+  if(!c){
+    p[8] = obj[c]
+    p[6] = p[6] ? p[6] + obj[c] : obj[c]
+  }else{
+    p[currentValue - 1] = obj[currentValue]
+  }
+  return p
+}, {})
 
 for(let i = 0; i < 256; i++){
-  inputObj = JSON.parse(JSON.stringify(process(inputObj)))
+  inputObj = Object.assign({}, process(inputObj))
 }
 
 console.log(Object.keys(inputObj).reduce((previousValue, currentValue) => previousValue += inputObj[currentValue], 0))
