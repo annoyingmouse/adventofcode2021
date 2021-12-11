7b5c6f8a6d02334336475b0d50e4e773906fcb6d
Dominic Myers <Dominic.Myers@spacenk.com>
2021-12-11 17:20:19 +0000

    Started Day 11 and working on a carousel

---
+++ ChristmasCarousel/index.js
@@ -0,0 +1,82 @@
+const previous = document.getElementById("previous")
+const next = document.getElementById("next")
+const imgs = [
+  {src: "imgs/village.jpg",
+    alt: "christmas village at night with snow and christmas tree"},
+  {src: "imgs/present.jpg",
+    alt: "white and gold wrapped present on white table with snowflake decorations"},
+  {src: "imgs/doggies.jpg",
+    alt: "small black dog and small beige dog looking out the window at snow next to Christmas wreath"}
+]
+const width = 359
+const delay = 500
+
+const carousel = document.getElementById('carousel')
+const track = document.createElement('div')
+carousel.appendChild(track)
+
+let slides = [...imgs].slice(-1)
+slides.push(...imgs)
+slides = [...slides, [...imgs].slice(0, 1)[0]]
+
+
+
+slides.forEach((slide, i) => {
+  const img = document.createElement('img')
+  console.log(slide.src)
+  img.setAttribute('src', slide.src)
+  img.setAttribute('alt', slide.alt)
+  img.dataset.index = i
+  track.appendChild(img)
+  if(i === 0 || i === slides.length - 1){
+    img.classList.add('cloned')
+  }
+  track.style.width = `${width *  5}px`
+  track.style.transition = `transform ${delay}ms ease 0s`
+  track.style.transform = `translate3d(-${width}px, 0, 0)`
+  track.dataset.position = `-${width}`
+  track.dataset.current = 1
+
+  carousel.style.width = `${width}px`
+  carousel.style.overflowX = 'hidden'
+})
+
+next.addEventListener('click', () => {
+
+  //     track.style.transition = 'none'
+  //     track.style.transform = `translate3d(0px, 0, 0)`
+  //     track.style.transform = `translate3d(-${width}px, 0, 0)`
+  //     track.style.transition = `transform ${delay}ms ease 0s`
+  //     track.dataset.current = 1
+  // }else{
+  track.style.transform = `translateX(${parseInt(track.dataset.position, 10) - width}px)`
+  track.dataset.position = `${parseInt(track.dataset.position, 10) - width}`
+  track.dataset.current = parseInt(track.dataset.current, 10) + 1
+  //}
+  if(parseInt(track.dataset.current, 10) === 3){
+    track.style.transition = 'none'
+    track.style.transform = `translate3d(0px, 0, 0)`
+    track.style.transform = `translate3d(-${width}px, 0, 0)`
+  }
+
+
+
+
+})
+
+previous.addEventListener('click', () => {
+  track.style.transform = `translateX(${parseInt(track.dataset.position, 10) + width}px)`
+  track.dataset.position = `${parseInt(track.dataset.position, 10) + width}`
+  track.dataset.current = parseInt(track.dataset.current, 10) + 1
+})
+
+
+// Task:
+// - Wire up the buttons to switch through the images in the imgs array.
+// - Make sure that the gallery works no matter how many images are added.
+// - Decide/implement what to do when you reach either end of the array - do nothing and disable buttons, loop back round to the other end, or something else?
+// - Remember to also update the alt tags.
+
+// Stretch goals:
+// - Add transitions for a smooth effect.
+// - Allow the user to zoom in and out of the images.
---
+++ day11/script.js
@@ -0,0 +1,68 @@
+const mini =
+  [
+    [1,1,1,1,1],
+    [1,9,9,9,1],
+    [1,9,1,9,1],
+    [1,9,9,9,1],
+    [1,1,1,1,1]
+  ]
+
+const test = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+
+const processStr = str => str.split(/\n/).reduce((t, c) => [...t, c.split('').map(s => Number(s))], [])
+const incrementAll = arr => {
+  for(let y = 0; y < arr.length; y++){
+    for(let x = 0; x < arr[y].length; x++){
+      arr[y][x] = arr[y][x] += 1
+    }
+  }
+  return arr
+}
+const incrementNeighbours = (y, x, arr) => {
+  const neighbours = [
+    [y - 1, x - 1], [y - 1, x],[y - 1, x + 1],
+    [y, x - 1],                [y + 1, x + 1],
+    [y + 1, x - 1], [y + 1, x],[y + 1, x + 1]
+  ]
+
+}
+
+
+const flash = arr => {
+  let inner = [...arr]
+  console.log(inner)
+  const nines = []
+  while(!!~inner.flat().indexOf(9)){
+    for(let y = 0; y < inner.length; y++){
+      for(let x = 0; x < inner[y].length; x++){
+        if(inner[y][x] === 9){
+          nines.push([y, x])
+          incrementNeighbours(y, x, inner)
+          inner[y][x] = 0
+        }
+      }
+    }
+  }
+
+}
+
+
+const part1 = str => {
+  let arr = processStr(str)
+  console.log(JSON.parse(JSON.stringify(arr)))
+  arr = incrementAll(arr)
+  console.log(JSON.parse(JSON.stringify(arr)))
+  //arr = flash(arr)
+}
+
+flash(mini)
+//part1(test)
